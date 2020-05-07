194c6df8e0fb2d9929e0d8221e610bb3a2670af4
kataderon <kataderon@gmail.com>
2020-05-07 14:02:14 +0300

    use pep-8

--- jobs/Scripts/base_functions.py
+++ jobs/Scripts/base_functions.py
@@ -22,221 +22,222 @@ LOGS_DIR = path.join(WORK_DIR, 'render_tool_logs')
 
 
 def logging(message):
-	print(' >>> [RPR TEST] [' +
-		  datetime.datetime.now().strftime('%H:%M:%S') + '] ' + message)
+    print(' >>> [RPR TEST] [' +
+          datetime.datetime.now().strftime('%H:%M:%S') + '] ' + message)
 
 
 def reportToJSON(case, render_time=0):
-	path_to_file = path.join(WORK_DIR, case['case'] + '_RPR.json')
+    path_to_file = path.join(WORK_DIR, case['case'] + '_RPR.json')
 
-	with open(path_to_file, 'r') as file:
-		report = json.loads(file.read())[0]
+    with open(path_to_file, 'r') as file:
+        report = json.loads(file.read())[0]
 
-	if case['status'] == 'inprogress':
-		report['test_status'] = 'passed'
-	else:
-		report['test_status'] = case['status']
+    if case['status'] == 'inprogress':
+        report['test_status'] = 'passed'
+    else:
+        report['test_status'] = case['status']
 
-	logging('Create report json ({{}} {{}})'.format(
-		case['case'], report['test_status']))
+    logging('Create report json ({{}} {{}})'.format(
+            case['case'], report['test_status']))
 
-	report['file_name'] = case['case'] + case.get('extension', '.jpg')
-	# TODO: render device may be incorrect (if it changes in case)
-	report['render_device'] = cmds.optionVar(q='RPR_DevicesName')[0]
-	report['tool'] = mel.eval('about -iv')
-	report['date_time'] = datetime.datetime.now().strftime('%m/%d/%Y %H:%M:%S')
-	report['render_version'] = mel.eval('getRPRPluginVersion()')
-	report['core_version'] = mel.eval('getRprCoreVersion()')
-	report['render_color_path'] = path.join('Color', report['file_name'])
-	report['render_time'] = render_time
-	report['test_group'] = TEST_TYPE
-	report['test_case'] = case['case']
-	report['difference_color'] = 0
-	report['script_info'] = case['script_info']
-	report['render_log'] = path.join('render_tool_logs', case['case'] + '.log')
-	report['scene_name'] = case.get('scene', '')
+    report['file_name'] = case['case'] + case.get('extension', '.jpg')
+    # TODO: render device may be incorrect (if it changes in case)
+    report['render_device'] = cmds.optionVar(q='RPR_DevicesName')[0]
+    report['tool'] = mel.eval('about -iv')
+    report['date_time'] = datetime.datetime.now().strftime('%m/%d/%Y %H:%M:%S')
+    report['render_version'] = mel.eval('getRPRPluginVersion()')
+    report['core_version'] = mel.eval('getRprCoreVersion()')
+    report['render_color_path'] = path.join('Color', report['file_name'])
+    report['render_time'] = render_time
+    report['test_group'] = TEST_TYPE
+    report['test_case'] = case['case']
+    report['difference_color'] = 0
+    report['script_info'] = case['script_info']
+    report['render_log'] = path.join('render_tool_logs', case['case'] + '.log')
+    report['scene_name'] = case.get('scene', '')
 
-	with open(path_to_file, 'w') as file:
-		file.write(json.dumps([report], indent=4))
+    with open(path_to_file, 'w') as file:
+        file.write(json.dumps([report], indent=4))
 
 
 def render_tool_log_path(name):
-	return path.join(LOGS_DIR, name + '.log')
+    return path.join(LOGS_DIR, name + '.log')
 
 
 def validateFiles():
-	logging('Repath scene')
-	# TODO: repath from folder with group
-	unresolved_files = cmds.filePathEditor(
-		query=True, listFiles='', unresolved=True, attributeOnly=True)
-	if unresolved_files:
-		for item in unresolved_files:
-			cmds.filePathEditor(item, repath=RES_PATH, recursive=True, ra=1)
+    logging('Repath scene')
+    # TODO: repath from folder with group
+    unresolved_files = cmds.filePathEditor(
+        query=True, listFiles='', unresolved=True, attributeOnly=True)
+    if unresolved_files:
+        for item in unresolved_files:
+            cmds.filePathEditor(item, repath=RES_PATH, recursive=True, ra=1)
 
 
 def enable_rpr():
-	if not cmds.pluginInfo('RadeonProRender', query=True, loaded=True):
-		cmds.loadPlugin('RadeonProRender', quiet=True)
-		logging('Load rpr')
-	if not cmds.pluginInfo('fbxmaya', query=True, loaded=True):
-		cmds.loadPlugin('fbxmaya', quiet=True)
-		logging('Load fbx')
+    if not cmds.pluginInfo('RadeonProRender', query=True, loaded=True):
+        cmds.loadPlugin('RadeonProRender', quiet=True)
+        logging('Load rpr')
+    if not cmds.pluginInfo('fbxmaya', query=True, loaded=True):
+        cmds.loadPlugin('fbxmaya', quiet=True)
+        logging('Load fbx')
 
 
 def rpr_render(case):
-	logging('Render image')
+    logging('Render image')
 
-	mel.eval('fireRender -waitForItTwo')
-	start_time = time.time()
-	mel.eval('renderIntoNewWindow render')
-	cmds.sysFile(path.join(WORK_DIR, 'Color'), makeDir=True)
-	test_case_path = path.join(WORK_DIR, 'Color', case['case'])
-	cmds.renderWindowEditor('renderView', edit=1,  dst='color')
-	cmds.renderWindowEditor('renderView', edit=1, com=1,
-							writeImage=test_case_path)
-	test_time = time.time() - start_time
+    mel.eval('fireRender -waitForItTwo')
+    start_time = time.time()
+    mel.eval('renderIntoNewWindow render')
+    cmds.sysFile(path.join(WORK_DIR, 'Color'), makeDir=True)
+    test_case_path = path.join(WORK_DIR, 'Color', case['case'])
+    cmds.renderWindowEditor('renderView', edit=1,  dst='color')
+    cmds.renderWindowEditor('renderView', edit=1, com=1,
+                            writeImage=test_case_path)
+    test_time = time.time() - start_time
 
-	reportToJSON(case, test_time)
+    reportToJSON(case, test_time)
 
 
 def prerender(case):
-	logging('Prerender')
-	scene = case.get('scene', '')
-	scene_name = cmds.file(q=True, sn=True, shn=True)
-	if scene_name != scene:
-		try:
-			cmds.file(scene, f=True, op='v=0;', prompt=False, iv=True, o=True)
-			validateFiles()
-			enable_rpr()
-		except:
-			logging("Can't load scene. Exit Maya")
-			cmds.evalDeferred('cmds.quit(abort=True)')
+    logging('Prerender')
+    scene = case.get('scene', '')
+    scene_name = cmds.file(q=True, sn=True, shn=True)
+    if scene_name != scene:
+        try:
+            cmds.file(scene, f=True, op='v=0;', prompt=False, iv=True, o=True)
+            validateFiles()
+            enable_rpr()
+        except:
+            logging("Can't load scene. Exit Maya")
+            cmds.evalDeferred('cmds.quit(abort=True)')
 
-	mel.eval('athenaEnable -ae false')
+    mel.eval('athenaEnable -ae false')
 
-	cmds.optionVar(rm='RPR_DevicesSelected')
-	cmds.optionVar(iva=('RPR_DevicesSelected',
-						(RENDER_DEVICE in ['gpu', 'dual'])))
-	cmds.optionVar(iva=('RPR_DevicesSelected',
-						(RENDER_DEVICE in ['cpu', 'dual'])))
+    cmds.optionVar(rm='RPR_DevicesSelected')
+    cmds.optionVar(iva=('RPR_DevicesSelected',
+                        (RENDER_DEVICE in ['gpu', 'dual'])))
+    cmds.optionVar(iva=('RPR_DevicesSelected',
+                        (RENDER_DEVICE in ['cpu', 'dual'])))
 
-	if RESOLUTION_X and RESOLUTION_Y:
-		cmds.setAttr('defaultResolution.width', RESOLUTION_X)
-		cmds.setAttr('defaultResolution.height', RESOLUTION_Y)
+    if RESOLUTION_X and RESOLUTION_Y:
+        cmds.setAttr('defaultResolution.width', RESOLUTION_X)
+        cmds.setAttr('defaultResolution.height', RESOLUTION_Y)
 
-	cmds.setAttr('defaultRenderGlobals.currentRenderer',
-				 type='string' 'FireRender')
+    cmds.setAttr('defaultRenderGlobals.currentRenderer',
+                 type='string' 'FireRender')
 
-	cmds.setAttr('defaultRenderGlobals.imageFormat', 8)
+    cmds.setAttr('defaultRenderGlobals.imageFormat', 8)
 
-	cmds.setAttr('RadeonProRenderGlobals.adaptiveThreshold', THRESHOLD)
-	cmds.setAttr(
-		'RadeonProRenderGlobals.completionCriteriaIterations', PASS_LIMIT)
-	cmds.setAttr('RadeonProRenderGlobals.samplesPerUpdate', SPU)
-	cmds.setAttr('RadeonProRenderGlobals.completionCriteriaSeconds', 0)
+    cmds.setAttr('RadeonProRenderGlobals.adaptiveThreshold', THRESHOLD)
+    cmds.setAttr(
+        'RadeonProRenderGlobals.completionCriteriaIterations', PASS_LIMIT)
+    cmds.setAttr('RadeonProRenderGlobals.samplesPerUpdate', SPU)
+    cmds.setAttr('RadeonProRenderGlobals.completionCriteriaSeconds', 0)
 
-	for function in case['functions']:
-		try:
-			if re.match('((^\S+|^\S+ \S+) = |^print|^if|^for|^with)', function):
-				exec(function)
-			else:
-				eval(function)
-		except Exception as e:
-			logging('Error "{{}}" with string "{{}}"'.format(e, function))
+    for function in case['functions']:
+        try:
+            if re.match('((^\S+|^\S+ \S+) = |^print|^if|^for|^with)', function):
+                exec(function)
+            else:
+                eval(function)
+        except Exception as e:
+            logging('Error "{{}}" with string "{{}}"'.format(e, function))
 
 
 def save_report(case):
-	logging('Save report without rendering for ' + case['case'])
+    logging('Save report without rendering for ' + case['case'])
 
-	if not os.path.exists(os.path.join(WORK_DIR, 'Color')):
-		os.makedirs(os.path.join(WORK_DIR, 'Color'))
+    if not os.path.exists(os.path.join(WORK_DIR, 'Color')):
+        os.makedirs(os.path.join(WORK_DIR, 'Color'))
 
-	work_dir = path.join(WORK_DIR, 'Color', case['case'] + '.jpg')
-	source_dir = path.join(WORK_DIR, '..', '..', '..',
-						   '..', 'jobs_launcher', 'common', 'img')
+    work_dir = path.join(WORK_DIR, 'Color', case['case'] + '.jpg')
+    source_dir = path.join(WORK_DIR, '..', '..', '..',
+                           '..', 'jobs_launcher', 'common', 'img')
 
-	if case['status'] == 'inprogress':
-		copyfile(path.join(source_dir, 'passed.jpg'), work_dir)
-	else:
-		copyfile(
-			path.join(source_dir, case['status'] + '.jpg'), work_dir)
+    if case['status'] == 'inprogress':
+        copyfile(path.join(source_dir, 'passed.jpg'), work_dir)
+    else:
+        copyfile(
+            path.join(source_dir, case['status'] + '.jpg'), work_dir)
 
-	enable_rpr()
+    enable_rpr()
 
-	reportToJSON(case)
+    reportToJSON(case)
 
 
 def case_function(case):
-	functions = {{
-		'prerender': prerender,
-		'save_report': save_report
-	}}
+    functions = {{
+        'prerender': prerender,
+        'save_report': save_report
+    }}
 
-	func = 'prerender'
+    func = 'prerender'
 
-	if case['functions'][0] == 'check_test_cases_success_save':
-		func = 'save_report'
-	else:
-		try:
-			projPath = os.path.join(RES_PATH, TEST_TYPE)
-			temp = os.path.join(projPath, case['scene'][:-3])
-			if os.path.isdir(temp):
-				projPath = temp
-			mel.eval('setProject("{{}}")'.format(projPath.replace('\\', '/')))
-		except:
-			logging("Can't set project in '" + projPath + "'")
-			cmds.evalDeferred('cmds.quit(abort=True)')
+    if case['functions'][0] == 'check_test_cases_success_save':
+        func = 'save_report'
+    else:
+        try:
+            projPath = os.path.join(RES_PATH, TEST_TYPE)
+            temp = os.path.join(projPath, case['scene'][:-3])
+            if os.path.isdir(temp):
+                projPath = temp
+            mel.eval('setProject("{{}}")'.format(projPath.replace('\\', '/')))
+        except:
+            logging("Can't set project in '" + projPath + "'")
+            cmds.evalDeferred('cmds.quit(abort=True)')
 
-	if case['status'] == 'fail' or case.get('number_of_tries', 1) == 2:	# 2- retries count
-		case['status'] = 'error'
-		func = 'save_report'
-	elif case['status'] == 'skipped':
-		func = 'save_report'
-	else:
-		case['number_of_tries'] = case.get('number_of_tries', 0) + 1
+    # 2- retries count
+    if case['status'] == 'fail' or case.get('number_of_tries', 1) == 2:
+        case['status'] = 'error'
+        func = 'save_report'
+    elif case['status'] == 'skipped':
+        func = 'save_report'
+    else:
+        case['number_of_tries'] = case.get('number_of_tries', 0) + 1
 
-	functions[func](case)
+    functions[func](case)
 
 
 # place for extension functions
 
 
 def main():
-	if not os.path.exists(os.path.join(WORK_DIR, LOGS_DIR)):
-		os.makedirs(os.path.join(WORK_DIR, LOGS_DIR))
+    if not os.path.exists(os.path.join(WORK_DIR, LOGS_DIR)):
+        os.makedirs(os.path.join(WORK_DIR, LOGS_DIR))
 
-	with open(path.join(WORK_DIR, 'test_cases.json'), 'r') as json_file:
-		cases = json.load(json_file)
+    with open(path.join(WORK_DIR, 'test_cases.json'), 'r') as json_file:
+        cases = json.load(json_file)
 
-	for case in cases:
-		if case['status'] in ['active', 'fail', 'skipped']:
-			if case['status'] == 'active':
-				case['status'] = 'inprogress'
+    for case in cases:
+        if case['status'] in ['active', 'fail', 'skipped']:
+            if case['status'] == 'active':
+                case['status'] = 'inprogress'
 
-			with open(path.join(WORK_DIR, 'test_cases.json'), 'w') as file:
-				json.dump(cases, file, indent=4)
+            with open(path.join(WORK_DIR, 'test_cases.json'), 'w') as file:
+                json.dump(cases, file, indent=4)
 
-			log_path = render_tool_log_path(case['case'])
-			if not path.exists(log_path):
-				with open(log_path, 'w'):
-					logging('Create log file for ' + case['case'])
-			cmds.scriptEditorInfo(historyFilename=log_path, writeHistory=True)
+            log_path = render_tool_log_path(case['case'])
+            if not path.exists(log_path):
+                with open(log_path, 'w'):
+                    logging('Create log file for ' + case['case'])
+            cmds.scriptEditorInfo(historyFilename=log_path, writeHistory=True)
 
-			logging(case['case'] + ' in progress')
+            logging(case['case'] + ' in progress')
 
-			start_time = datetime.datetime.now()
-			case_function(case)
-			case_time = (datetime.datetime.now() - start_time).total_seconds()
-			case['time_taken'] = case_time
+            start_time = datetime.datetime.now()
+            case_function(case)
+            case_time = (datetime.datetime.now() - start_time).total_seconds()
+            case['time_taken'] = case_time
 
-			if case['status'] == 'inprogress':
-				case['status'] = 'done'
-				logging(case['case'] + ' done')
+            if case['status'] == 'inprogress':
+                case['status'] = 'done'
+                logging(case['case'] + ' done')
 
-			with open(path.join(WORK_DIR, 'test_cases.json'), 'w') as file:
-				json.dump(cases, file, indent=4)
+            with open(path.join(WORK_DIR, 'test_cases.json'), 'w') as file:
+                json.dump(cases, file, indent=4)
 
-	cmds.evalDeferred('cmds.quit(abort=True)')
+    cmds.evalDeferred('cmds.quit(abort=True)')
 
 
 main()
--- jobs/Scripts/refactor_logs.py
+++ jobs/Scripts/refactor_logs.py
@@ -5,56 +5,62 @@ import datetime
 
 
 errors = [
-	{'error': 'rprCachingShadersWarningWindow',
-	 'message': 'Render cache built during cases'},
-	{'error': 'Error: Radeon ProRender: IO error',
-	 'message': 'Some files/textures are missing'},
-	 {'error': 'Error occurred during execution of MEL script',
-	 'message': 'Error occurred during execution of MEL script'}
+    {'error': 'rprCachingShadersWarningWindow',
+     'message': 'Render cache built during cases'},
+    {'error': 'Error: Radeon ProRender: IO error',
+     'message': 'Some files/textures are missing'},
+    {'error': 'Error occurred during execution of MEL script',
+     'message': 'Error occurred during execution of MEL script'}
 ]
 
 
 def createArgsParser():
-	parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
-	parser.add_argument('--output', required=True, metavar='<dir>')
+    parser.add_argument('--output', required=True, metavar='<dir>')
 
-	return parser
+    return parser
 
 
 def main(args):
-	work_dir = os.path.abspath(args.output).replace('\\', '/')
-	files = [f for f in os.listdir(
-		work_dir) if os.path.isfile(os.path.join(work_dir, f))]
-	files = [f for f in files if 'renderTool' in f]
+    work_dir = os.path.abspath(args.output).replace('\\', '/')
+    files = [f for f in os.listdir(
+        work_dir) if os.path.isfile(os.path.join(work_dir, f))]
+    files = [f for f in files if 'renderTool' in f]
 
-	logs = ''
+    logs = ''
 
-	for f in files:
-		logs += '\n\n\n----------LOGS FROM FILE ' + f + '----------\n\n\n'
-		with open(os.path.realpath(os.path.join(os.path.abspath(args.output).replace('\\', '/'), f))) as log:
-			logs += log.read()
-		os.remove(os.path.realpath(os.path.join(
-			os.path.abspath(args.output).replace('\\', '/'), f)))
+    for f in files:
+        logs += '\n\n\n----------LOGS FROM FILE ' + f + '----------\n\n\n'
+        with open(os.path.realpath(os.path.join(os.path.abspath(args.output).replace('\\', '/'), f))) as log:
+            logs += log.read()
+        os.remove(os.path.realpath(os.path.join(
+            os.path.abspath(args.output).replace('\\', '/'), f)))
 
-	with open(os.path.realpath(os.path.join(os.path.abspath(args.output).replace('\\', '/'), 'renderTool.log')), 'w') as f:
-		for error in errors:
-			if error['error'] in logs:
-				f.write('[Error] {}\n'.format(error['message']))
+    with open(os.path.realpath(os.path.join(os.path.abspath(args.output).replace('\\', '/'), 'renderTool.log')), 'w') as f:
+        for error in errors:
+            if error['error'] in logs:
+                f.write('[Error] {}\n'.format(error['message']))
 
-		f.write('\n\nCases statuses from test_cases.json\n\n')
+        f.write('\n\nCases statuses from test_cases.json\n\n')
 
-		cases = json.load(open(os.path.realpath(
-			os.path.join(os.path.abspath(args.output).replace('\\', '/'), 'test_cases.json'))))
+        cases = json.load(open(os.path.realpath(
+            os.path.join(os.path.abspath(args.output).replace('\\', '/'), 'test_cases.json'))))
 
-		f.write('Active cases: {}\n'.format(len([n for n in cases if n['status'] == 'active'])))
-		f.write('Inprogress cases: {}\n'.format(len([n for n in cases if n['status'] == 'inprogress'])))
-		f.write('Fail cases: {}\n'.format(len([n for n in cases if n['status'] == 'fail'])))
-		f.write('Error cases: {}\n'.format(len([n for n in cases if n['status'] == 'error'])))
-		f.write('Done cases: {}\n'.format(len([n for n in cases if n['status'] == 'done'])))
-		f.write('Skipped cases: {}\n\n'.format(len([n for n in cases if n['status'] == 'skipped'])))
+        f.write('Active cases: {}\n'.format(
+            len([n for n in cases if n['status'] == 'active'])))
+        f.write('Inprogress cases: {}\n'.format(
+            len([n for n in cases if n['status'] == 'inprogress'])))
+        f.write('Fail cases: {}\n'.format(
+            len([n for n in cases if n['status'] == 'fail'])))
+        f.write('Error cases: {}\n'.format(
+            len([n for n in cases if n['status'] == 'error'])))
+        f.write('Done cases: {}\n'.format(
+            len([n for n in cases if n['status'] == 'done'])))
+        f.write('Skipped cases: {}\n\n'.format(
+            len([n for n in cases if n['status'] == 'skipped'])))
 
-		f.write('''\tPossible case statuses:\nActive: Case will be executed.
+        f.write('''\tPossible case statuses:\nActive: Case will be executed.
 Inprogress: Case is in progress (if maya was crashed, case will be inprogress).
 Fail: Maya was crashed during case. Fail report will be created.
 Error: Maya was crashed during case. Fail report is already created.
@@ -63,18 +69,20 @@ Skipped: Case will be skipped. Skip report will be created.\n
 Case\t\tStatus\tTime\tTries
 \n''')
 
-		total_time = 0
+        total_time = 0
 
-		for case in cases:
-			case_time = '{:.2f}'.format(case.get("time_taken", 0))
-			f.write('{}\t{}\t{}\t{}\n'.format(case['case'], case['status'], case_time, case.get('number_of_tries', 1)))
-			total_time += float(case.get('time_taken', '0'))
+        for case in cases:
+            case_time = '{:.2f}'.format(case.get("time_taken", 0))
+            f.write('{}\t{}\t{}\t{}\n'.format(
+                case['case'], case['status'], case_time, case.get('number_of_tries', 1)))
+            total_time += float(case.get('time_taken', '0'))
 
-		f.write('Time taken: ' + str(datetime.datetime.utcfromtimestamp(total_time).strftime('%H:%M:%S')))
+        f.write('Time taken: ' +
+                str(datetime.datetime.utcfromtimestamp(total_time).strftime('%H:%M:%S')))
 
-		f.write(logs)
+        f.write(logs)
 
 
 if __name__ == '__main__':
-	args = createArgsParser().parse_args()
-	main(args)
+    args = createArgsParser().parse_args()
+    main(args)
