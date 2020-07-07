f8efc99bc826b47df2a7457f286dd3aea7348f70
Tarasenko Daniil <dtarasenko@luxoft.com>
2020-07-08 00:45:59 +0300

    fix logs for cache building

--- scripts/build_rpr_cache.sh
+++ scripts/build_rpr_cache.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 export PYTHONPATH=$PWD/../jobs/Scripts:$PYTHONPATH
 export MAYA_SCRIPT_PATH=$PWD/../jobs/Scripts:$MAYA_SCRIPT_PATH
-export MAYA_CMD_FILE_OUTPUT=$PWD/../Work/Results/Maya
+export MAYA_CMD_FILE_OUTPUT=$PWD/../Work/Results/Maya/renderTool.cb.log
 
 TOOL=${1:-2020}
 
-/Applications/Autodesk/Maya${TOOL}/Maya.app/Contents/bin/Render -r FireRender -log "$MAYA_CMD_FILE_OUTPUT/renderTool.cb.log" -rd "$MAYA_CMD_FILE_OUTPUT" -im "cache_building" -of jpg "$CIS_TOOLS/../TestResources/MayaAssets/cache.mb"
+mkdir -p $PWD/../Work/Results/Maya
+
+/Applications/Autodesk/Maya${TOOL}/Maya.app/Contents/bin/Render -r FireRender -log "$MAYA_CMD_FILE_OUTPUT/renderTool.cb.stdout.log" -rd "$MAYA_CMD_FILE_OUTPUT" -im "cache_building" -of jpg "$CIS_TOOLS/../TestResources/MayaAssets/cache.mb"
