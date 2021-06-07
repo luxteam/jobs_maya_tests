e65a6c6a828c69d38b3ac85ec1d879305156e922
Wieceslaw <slava.astra@yahoo.com>
2021-06-07 16:28:24 +0300

    Fix + Image saving

--- jobs/Scripts/cache_building.py
+++ jobs/Scripts/cache_building.py
@@ -1,5 +1,6 @@
 import maya.cmds as cmds
 import maya.mel as mel
+import os
 
 
 def main():
@@ -14,11 +15,19 @@ def main():
     print("Plugin has been loaded")
 
     try:
+        print("Preparation for rendering")
         cmds.setAttr('defaultRenderGlobals.currentRenderer', type='string' 'FireRender')
-        cmds.setAttr("RadeonProRenderGlobals.completionCriteriaSeconds", 0)
-        cmds.setAttr("RadeonProRenderGlobals.completionCriteriaIterations", 0)
+        cmds.setAttr("RadeonProRenderGlobals.completionCriteriaSeconds", 30)
+        cmds.setAttr('defaultRenderGlobals.imageFormat', 8)
+        
         mel.eval('fireRender -waitForItTwo')
         mel.eval('renderIntoNewWindow render')
+        
+        results_dir = os.path.abspath(os.path.join(os.getcwd(), '..', '..', 'Work', 'Results', 'Maya'))
+        cmds.sysFile(results_dir, makeDir=True)
+        test_case_path = os.path.join(results_dir, 'cache_building')
+        cmds.renderWindowEditor('renderView', edit=1,  dst='color')
+        cmds.renderWindowEditor('renderView', edit=1, com=1, writeImage=test_case_path)
         print("Render has been finished")
     except Exception as err:
         print("Error during rendering. {}".format(str(err)))
--- scripts/build_rpr_cache.bat
+++ scripts/build_rpr_cache.bat
@@ -5,4 +5,4 @@ set MAYA_CMD_FILE_OUTPUT=%CD%\..\Work\Results\Maya\renderTool.cb.log
 set TOOL=%1
 if not defined TOOL set TOOL=2020
 
-"C:\Program Files\Autodesk\Maya%TOOL%\bin\maya.exe" -command "python(\"import cache_building\")" -file "С:\\TestResources\\rpr_maya_autotests_assets\\material_baseline.mb"
+"C:\Program Files\Autodesk\Maya%TOOL%\bin\maya.exe" -command "python(\"import cache_building\")" -file "C:\\TestResources\\rpr_maya_autotests_assets\\material_baseline.mb"
