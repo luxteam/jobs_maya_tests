55ba6e0ef4606376fdea6adbcc6ffdc5d9b2cb4e
Wieceslaw <slava.astra@yahoo.com>
2021-06-08 18:04:11 +0300

    Remove log parameter and print MAYA_CMD_FILE_OUTPUT content to console

--- scripts/build_rpr_cache.bat
+++ scripts/build_rpr_cache.bat
@@ -1,14 +1,13 @@
 set PYTHONPATH=%CD%\..\jobs\Scripts;%PYTHONPATH%
 set MAYA_SCRIPT_PATH=%CD%\..\jobs\Scripts;%MAYA_SCRIPT_PATH%
+set MAYA_CMD_FILE_OUTPUT=%CD%\..\templog
 
 set TOOL=%1
 set ENGINE=%2
-set LOG_FILE=%3
 
 if not defined TOOL set TOOL=2020
 if not defined ENGINE set ENGINE=Tahoe
-if not defined LOG_FILE set LOG_FILE=renderTool.cb.log
-
-set MAYA_CMD_FILE_OUTPUT=%CD%\%LOG_FILE%
 
 "C:\Program Files\Autodesk\Maya%TOOL%\bin\maya.exe" -command "python(\"import cache_building\")" -file "C:\\TestResources\\rpr_maya_autotests_assets\\material_baseline.mb"
+
+type %MAYA_CMD_FILE_OUTPUT%
--- scripts/build_rpr_cache.sh
+++ scripts/build_rpr_cache.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 export PYTHONPATH=$PWD/../jobs/Scripts:$PYTHONPATH
 export MAYA_SCRIPT_PATH=$PWD/../jobs/Scripts:$MAYA_SCRIPT_PATH
+export MAYA_CMD_FILE_OUTPUT=$PWD/../templog
 
 TOOL=${1:-2020}
 ENGINE=${2:-Tahoe}
-LOG_FILE=${3:-renderTool.cb.log}
-
-export MAYA_CMD_FILE_OUTPUT=$PWD/$LOG_FILE
 
 maya${TOOL} -command "python(\"import cache_building\")" -file "$CIS_TOOLS/../TestResources/rpr_maya_autotests_assets/material_baseline.mb"
+cat $MAYA_CMD_FILE_OUTPUT
