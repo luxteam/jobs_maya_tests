118385af0bdd5df6db449859457230fe7d53b3f6
Wieceslaw <48382010+NISHIY-EKSDEE@users.noreply.github.com>
2021-06-15 14:54:29 +0300

    GUI cache building + engine option (#331)

--- scripts/build_rpr_cache.bat
+++ scripts/build_rpr_cache.bat
@@ -1,8 +1,13 @@
 set PYTHONPATH=%CD%\..\jobs\Scripts;%PYTHONPATH%
 set MAYA_SCRIPT_PATH=%CD%\..\jobs\Scripts;%MAYA_SCRIPT_PATH%
-set MAYA_CMD_FILE_OUTPUT=%CD%\..\Work\Results\Maya
+set MAYA_CMD_FILE_OUTPUT=%CD%\..\templog
 
 set TOOL=%1
-if not defined TOOL set TOOL=2020
+set ENGINE=%2
 
-"C:\\Program Files\\Autodesk\\Maya%TOOL%\\bin\\Render.exe" -r FireRender -log "%MAYA_CMD_FILE_OUTPUT%\renderTool.cb.log" -rd "%MAYA_CMD_FILE_OUTPUT%" -im "cache_building" -of jpg "C:\\TestResources\\rpr_maya_autotests_assets\\material_baseline.mb"
+if not defined TOOL set TOOL=2020
+if not defined ENGINE set ENGINE=Tahoe
+
+"C:\Program Files\Autodesk\Maya%TOOL%\bin\maya.exe" -command "python(\"import cache_building\")" -file "C:\\TestResources\\rpr_maya_autotests_assets\\material_baseline.mb"
+
+type %MAYA_CMD_FILE_OUTPUT%
--- scripts/build_rpr_cache.sh
+++ scripts/build_rpr_cache.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 export PYTHONPATH=$PWD/../jobs/Scripts:$PYTHONPATH
 export MAYA_SCRIPT_PATH=$PWD/../jobs/Scripts:$MAYA_SCRIPT_PATH
-export MAYA_CMD_FILE_OUTPUT=$PWD/../Work/Results/Maya
+export MAYA_CMD_FILE_OUTPUT=$PWD/../templog
 
 TOOL=${1:-2020}
+export ENGINE=${2:-Tahoe}
 
-/Applications/Autodesk/Maya${TOOL}/Maya.app/Contents/bin/Render -r FireRender -log "$MAYA_CMD_FILE_OUTPUT/renderTool.cb.log" -rd "$MAYA_CMD_FILE_OUTPUT" -im "cache_building" -of jpg "$CIS_TOOLS/../TestResources/rpr_maya_autotests_assets/material_baseline.mb"
+maya${TOOL} -command "python(\"import cache_building\")" -file "$CIS_TOOLS/../TestResources/rpr_maya_autotests_assets/material_baseline.mb"
+
+echo "--------Script editor log--------"
+cat $MAYA_CMD_FILE_OUTPUT
