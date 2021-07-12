392ed583a27f81743a5fc5d8466b4d2e69009ebc
Ilia Nemankov <INemankov@luxoft.com>
2021-07-12 14:46:47 +0300

    Update for Apple M1

--- scripts/run.sh
+++ scripts/run.sh
@@ -12,7 +12,9 @@ ENGINE=${10:-Tahoe}
 RETRIES=${11:-2}
 UPDATE_REFS=${12:-No}
 
-python3.9 -m pip install --user -r ../jobs_launcher/install/requirements.txt
+if [ $CIS_RENDER_DEVICE -ne "Apple M1" ]; then
+    python3.9 -m pip install --user -r ../jobs_launcher/install/requirements.txt
+fi
 
 python3.9 ../jobs_launcher/executeTests.py --file_filter $FILE_FILTER --test_filter $TESTS_FILTER --tests_root ../jobs --work_root ../Work/Results --work_dir Maya --cmd_variables Tool "/Applications/Autodesk/maya${TOOL}/Maya.app/Contents/bin" RenderDevice "$RENDER_DEVICE" ResPath "$CIS_TOOLS/../TestResources/rpr_maya_autotests_assets" PassLimit $ITER rx $RX ry $RY SPU $SPU threshold $THRESHOLD engine $ENGINE retries $RETRIES UpdateRefs $UPDATE_REFS
 
